ad64329f5bf567423ef33a09bae2ac448aaa15d9
Evzen230 <119056309+Evzen230@users.noreply.github.com>
2026-02-05 20:25:04 +0100

    Replace keep_alive with start_web

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import os
 from discord.ext import commands
 import asyncio
-from keep_alive import keep_alive
+from keep_alive import start_web
 from random import random
 from operator import itemgetter
 from discord.ui import View, Button
@@ -18,7 +18,7 @@ import re
 from config import *
 
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-keep_alive()
+start_web()
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
